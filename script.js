9f622035b1cd517778562142b58091bbcc268641
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-06-13 12:09:24 -0400

    Write and debug playRound function

--- script.js
+++ script.js
@@ -4,40 +4,17 @@ let computerWins = 0;
 let userWins = 0;
 
 // repeat 5 times:
+  // prompt user for input of "Rock", "Paper", or "Scissors" and store it in playerSelection
+  const playerSelection = "rock";
+  // while playerSelection !== "Rock", "Paper", or "Scissors" (case insensitive)
+    // re-prompt user
+
   // function to randomly select computer's choice and store it in computerSelection
   const computerSelection = getComputerChoice();
   //console.log(computerSelection);
 
-  // prompt user for input of "Rock", "Paper", or "Scissors" and store it in userSelection
-  // while userSelection !== "Rock", "Paper", or "Scissors" (case insensitive)
-    // re-prompt user
-
-  // if userSelection is same as computer selection
-    // print `It's a tie! ${userSelection} is the same as ${computerSelection}!`
-
-  // if userSelection is "rock"
-    // if computerSelection is "paper"
-      // print "You lose! Paper beats Rock!"
-      // add 1 to computerWins
-    // if computerSelection is "scissors"
-      // print "You win! Rock beats Scissors!"
-      // add 1 to userWins
-
-  // if userSelection is "paper"
-    // if computerSelection is "scissors"
-      // print "You lose! Scissors beats Paper!"
-      // add 1 to computerWins
-    // if computerSelection is "rock"
-      // print "You win! Paper beats Rock!"
-      // add 1 to userWins
-
-  // if userSelection is "scissors"
-    // if computerSelection is "rock"
-      // print "You lose! Rock beats Scissors!"
-      // add 1 to computerWins
-    // if computerSelection is "paper"
-      // print "You win! Scissors beats Paper!"
-      // add 1 to userWins
+  // function to play a single round of Rock Paper Scissors. Takes playerSelection and computerSelection as parameters, returns a string that declares winner of the round (e.g. "You Lose! Paper beats Rock")
+  console.log(playRound(playerSelection, computerSelection));
 
 // if userWins > computerWins
   // print "Congratulations! You win!"
@@ -46,6 +23,7 @@ let userWins = 0;
 // else
   // print "It's a tie!"
 
+  
 
 // function to randomly select computer's choice and store it in computerSelection
 function getComputerChoice() {
@@ -57,20 +35,93 @@ function getComputerChoice() {
     // if random integer is 0
     case 0:
       // computerSelection = "Rock"
-      selection = "Rock";
+      selection = "rock";
       break;
     // if random integer is 1
     case 1:
       // computerSelection = "Paper"
-      selection = "Paper";
+      selection = "paper";
       break;
     // if random integer is 2
     case 2:
       // computerSelection = "Scissors"
-      selection = "Scissors";
+      selection = "scissors";
       break;
   }
   
   // return selection
   return selection;
+}
+
+// function to play a single round of Rock Paper Scissors. Takes playerSelection and computerSelection as parameters, returns a string that declares winner of the round (e.g. "You Lose! Paper beats Rock")
+function playRound(playerSelection, computerSelection) {
+  // if playerSelection is same as computer selection
+  if (playerSelection === computerSelection) {
+    // capitalize playerSelection
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    // capitalize computerSelection
+    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1);
+    // return `It's a tie! ${playerSelection} is the same as ${computerSelection}!`
+    return `It's a tie! ${playerSelection} is the same as ${computerSelection}.`;
+  }
+
+  // if playerSelection is "rock"
+  else if (playerSelection === "rock") {
+    switch (computerSelection) {
+      // if computerSelection is "paper"
+      case "paper":
+        // add 1 to computerWins
+        computerWins++;
+        // return "You lose! Paper beats Rock!"
+        return "You lose! Paper beats Rock!";
+        break;
+      // if computerSelection is "scissors"
+      case "scissors":
+        // add 1 to userWins
+        userWins++;
+        // return "You win! Rock beats Scissors!"
+        return "You win! Rock beats Scissors!";
+        break;
+    }
+  }
+
+  // if playerSelection is "paper"
+  else if (playerSelection === "paper") {
+    switch (computerSelection) {
+      // if computerSelection is "scissors"
+      case "paper":
+        // add 1 to computerWins
+        computerWins++;
+        // return "You lose! Scissors beats Paper!"
+        return "You lose! Scissors beats Paper!";
+        break;
+      // if computerSelection is "rock"
+      case "rock":
+        // add 1 to userWins
+        userWins++;
+        // return "You win! Paper beats Rock!"
+        return "You win! Paper beats Rock!";
+        break;
+    }
+  }
+
+  // if playerSelection is "scissors"
+  else if (playerSelection === "scissors") {
+    switch (computerSelection) {
+    // if computerSelection is "rock"
+    case "rock":
+      // add 1 to computerWins
+      computerWins++;
+      // return "You lose! Rock beats Scissors!"
+      return "You lose! Rock beats Scissors!";
+      break;
+    // if computerSelection is "paper"
+    case "paper":
+      // add 1 to userWins
+      userWins++;
+      // return "You win! Scissors beats Paper!"
+      return "You win! Scissors beats Paper!";
+      break;
+    }
+  }
 }
