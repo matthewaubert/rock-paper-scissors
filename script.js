91cf3b7eecb64a55c71de0460b7ca9644b63d5eb
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-21 14:36:20 -0400

    Evoke playRound when user clicks button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let userScore = 0;
 
 const choices = ["rock", "paper", "scissors"];
 
-game();
+// game();
 
 
 
@@ -36,69 +36,67 @@ function game() {
 // function to randomly select computer's choice and store it in computerSelection
 function getComputerChoice() {
   let selection;
-  // get random integer between 0 and 2
-  let randNum = Math.floor(Math.random() * 3);
-  switch (randNum) {
-    case 0:
-      selection = choices[0];
-      break;
-    case 1:
-      selection = choices[1];
-      break;
-    case 2:
-      selection = choices[2];
-      break;
-  }
+  let randNum = Math.floor(Math.random() * 3); // get random integer between 0 and 2
+  selection = choices[randNum]; // computer selection is randNum index in choices array
   
   return selection;
 }
 
-// function to play a single round of Rock Paper Scissors.
-// Takes playerSelection and computerSelection as parameters,
+// function to play a single round of Rock Paper Scissors; input: playerSelection
 // returns a string that declares winner of the round (e.g. "You Lose! Paper beats Rock")
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+
+  if (playerSelection === computerSelection) { // if player and computer make same choice
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
     computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1);
     return `It's a tie! ${playerSelection} is the same as ${computerSelection}.`;
   }
 
-  else if (playerSelection === choices[0]) {
+  else if (playerSelection === choices[0]) { // if player chooses rock
     switch (computerSelection) {
-      case choices[1]:
+      case choices[1]: // if computer chooses paper
         computerScore++;
         return "You lose! Paper beats Rock!";
         break;
-      case choices[2]:
+      case choices[2]: // if computer chooses scissors
         userScore++;
         return "You win! Rock beats Scissors!";
         break;
     }
   }
 
-  else if (playerSelection === choices[1]) {
+  else if (playerSelection === choices[1]) { // if player chooses paper
     switch (computerSelection) {
-      case choices[2]:
+      case choices[2]: // if computer chooses scissors
         computerScore++;
         return "You lose! Scissors beats Paper!";
         break;
-      case choices[0]:
+      case choices[0]: // if computer chooses rock
         userScore++;
         return "You win! Paper beats Rock!";
         break;
     }
   }
 
-  else if (playerSelection === choices[2]) {
+  else if (playerSelection === choices[2]) { // if player chooses scissors
     switch (computerSelection) {
-    case choices[0]:
+    case choices[0]: // if computer chooses rock
       computerScore++;
       return "You lose! Rock beats Scissors!";
       break;
-    case choices[1]:
+    case choices[1]: // if computer chooses paper
       userScore++;
       return "You win! Scissors beats Paper!";
       break;
     }
   }
 }
+
+
+const buttons = document.querySelectorAll('#buttons button'); // select buttons
+buttons.forEach(button => { // iterate over buttons
+  // add a click event listener: invoke playRound function with correct playerSelection
+  let playerSelection = button.innerText;
+  button.addEventListener('click', () => console.log(playRound(playerSelection)));
+});
