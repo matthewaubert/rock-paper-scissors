8dbb00d42cb4f26f94db13316ecc8f03fd26f30a
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-21 16:01:27 -0400

    Display running score, announce winner at 5 points

--- script.js
+++ script.js
@@ -4,34 +4,8 @@ let userScore = 0;
 
 const choices = ["rock", "paper", "scissors"];
 
-// game();
 playGame();
 
-
-// function to play a 5-round game of Rock Paper Scissors, keep score, and report the winner
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     let playerSelection;
-//     // prompt user for input of "Rock", "Paper", or "Scissors" and store it in playerSelection
-//     do {
-//       playerSelection = prompt("Want to play Rock Paper Scissors?\nChoose 'rock', 'paper', or 'scissors'").toLowerCase();
-//     } while (!choices.includes(playerSelection));
-
-//     let computerSelection = getComputerChoice();
-
-//     console.log(playRound(playerSelection, computerSelection));
-//   }
-
-//   if (userScore > computerScore) {
-//     console.log("Congratulations! You win!");
-//   }
-//   else if (computerScore > userScore) {
-//     console.log("Sorry, you lose!");
-//   }
-//   else {
-//     console.log("It's a tie!");
-//   }
-// }
 
 function playGame() {
   // click buttons to play a round
@@ -56,6 +30,7 @@ function getComputerChoice() {
 // returns a string that declares winner of the round (e.g. "You Lose! Paper beats Rock")
 function playRound(playerSelection) {
   let computerSelection = getComputerChoice();
+  let winner = undefined;
   let msg;
 
   if (playerSelection === computerSelection) { // if player and computer make same choice
@@ -67,11 +42,11 @@ function playRound(playerSelection) {
   else if (playerSelection === choices[0]) { // if player chooses rock
     switch (computerSelection) {
       case choices[1]: // if computer chooses paper
-        computerScore++;
+        winner = 1;
         msg = "You lose! Paper beats Rock!";
         break;
       case choices[2]: // if computer chooses scissors
-        userScore++;
+        winner = 0;
         msg = "You win! Rock beats Scissors!";
         break;
     }
@@ -80,11 +55,11 @@ function playRound(playerSelection) {
   else if (playerSelection === choices[1]) { // if player chooses paper
     switch (computerSelection) {
       case choices[2]: // if computer chooses scissors
-        computerScore++;
+        winner = 1;
         msg = "You lose! Scissors beats Paper!";
         break;
       case choices[0]: // if computer chooses rock
-        userScore++;
+        winner = 0;
         msg = "You win! Paper beats Rock!";
         break;
     }
@@ -93,17 +68,26 @@ function playRound(playerSelection) {
   else if (playerSelection === choices[2]) { // if player chooses scissors
     switch (computerSelection) {
     case choices[0]: // if computer chooses rock
-      computerScore++;
+      winner = 1;
       msg = "You lose! Rock beats Scissors!";
       break;
     case choices[1]: // if computer chooses paper
-      userScore++;
+      winner = 0;
       msg = "You win! Scissors beats Paper!";
       break;
     }
   }
 
   displayResults(msg); // display results of round
+  updateScore(winner); // update score
+
+  if (userScore === 5) { // if user reaches 5 points
+    announceWinner(0); // announce winner
+    return; // end game
+  } else if (computerScore === 5) { // if computer reaches 5 points
+    announceWinner(1);
+    return;
+  }
 }
 
 // function to display results of round; input: message
@@ -111,3 +95,36 @@ function displayResults(msg) {
   const results = document.querySelector('#results'); // select results id
   results.innerText = msg;
 }
+
+// change score displayed after button is clicked; input: 0 for user win, 1 for computer win
+function updateScore(winner) {
+  if (winner === 0) { // if user is winner (0)
+    userScore++; // increment user score
+    const userScoreDisplay = document.querySelector('#user'); // select user id
+    userScoreDisplay.innerText = `You: ${userScore}`; // update user score display text
+  } else if (winner === 1) { // if computer is winner (1)
+    computerScore++; // increment computer score
+    const compScoreDisplay = document.querySelector('#comp'); // select comp id
+    compScoreDisplay.innerText = `Computer: ${computerScore}`; // update user score display text
+  }
+  // if tie (winner is undefined), do nothing
+}
+
+// announce winner of game; input: 0 for user win, 1 for computer win
+function announceWinner(winner) {
+  const winnerDisplay = document.querySelector('#winner'); // select winner div
+
+  if (winner === 0) { // if user is winner (0)
+    // add text to winner div: "Congratulations! You win!"
+    winnerDisplay.innerText = "Congratulations! You win!";
+  } else { // if computer is winner (1)
+    // add text to winner div: "Sorry, you lose!"
+    winnerDisplay.innerText = "Sorry, you lose!"
+  }
+}
+
+//   if (userScore > computerScore) {
+//     console.log("Congratulations! You win!");
+//   }
+//   else if (computerScore > userScore) {
+//     console.log("Sorry, you lose!");
